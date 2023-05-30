f2e62153611fb0213db9c1366bc84fa7b5491a92
RolandoAriManggara <qyarafisa08@gmail.com>
2023-05-30 11:06:16 +0700

    hotel app

---
+++ app/src/main/java/com/rolandoarimanggara202102336/hotelapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.rolandoarimanggara202102336.hotelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView _imageView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        _imageView1 = (ImageView) findViewById(R.id.imageView1);
+
+        String imageUrl = "https://www.balitoursclub.net/wp-content/uploads/2019/02/Hotel-di-pinggir-pantai-Seminyak-Bali.jpg";
+        Picasso.with(this).load(imageUrl).into(_imageView1);
+    }
+}
